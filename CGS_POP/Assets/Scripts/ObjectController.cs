624d33ac860dda1450ca47dad46d9d17a637f0fe
Matthew Churchyard <Matthew2.Churchyard@live.uwe.ac.uk>
2019-02-02 17:00:57 +0000

    Set up basics for the Scene
    Has a playercontroller which needs to be filled, scene contains player and five different coloured cubes

---
+++ CGS_POP/Assets/Scripts/ObjectController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+	//this will later have to be updated to find an array of playercontroller scripts, as there should be four in scene which all need to be reacted to
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		GameObject Player = GameObject.Find("Player");
+		PlayerController pc = Player.GetComponent<PlayerController>();
+		Transform tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	//contains action to be performed when player1 presses their button - GROW
+	void Player1Action() {
+
+	}
+
+	//contains action to be performed when player2 presses their button - PULL
+	void Player2Action() {
+
+	}
+
+	//contains action to be performed when player3 presses their button - SWITCH
+	void Player3Action() {
+
+	}
+
+	//contains action to be performed when player4 presses their button - FLOAT
+	void Player4Action() {
+
+	}
+
+}
---
+++ CGS_POP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	//CONTENTS OF THIS SCRIPT:
+	//Controls for players
+	//Controls for pressing coloured buttons, which then searches the scene for relevant shapes and tells them to trigger their functions for as long as the button is held
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
