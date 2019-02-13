4733519632814da5f6882a22039ad8de63f97747
Nshan Petrosyan <nshanpetrosyann@gmail.com>
2019-02-13 09:05:46 +0200

    Another trap with button
    Added a side wall trap with a button trigger

---
+++ CGS_POP/Assets/Scripts/Traps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    public Animator ani;
+ 
+       
+  
+    //NSHAN
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani.enabled = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //plays animation when collider is hit
+   public void OnTriggerEnter()
+    {
+        ani.enabled = true;
+        
+    }
+}
