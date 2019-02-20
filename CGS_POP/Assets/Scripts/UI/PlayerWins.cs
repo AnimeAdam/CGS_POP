70fe7d217a5fc6b8db924e6722259272e57f1d8f
AnimeAdam <Adam.Szlamp@gmail.com>
2019-02-20 04:01:26 +0000

    Simple End Level flag

---
+++ CGS_POP/Assets/Scripts/UI/PlayerWins.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerWins : MonoBehaviour
+{
+    private bool winner = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (winner)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                int playerNum = other.GetComponent<GamePlayer>().playerId + 1;
+                //GameObject playerWinner = GameObject.Find("Canvas/PlayerWinner");
+                Text playerText = GameObject.Find("Canvas/PlayerWinner").GetComponent<Text>();
+                playerText.text = "Player " + playerNum + " WINS";
+                winner = false;
+            }
+        }   
+    }
+}
