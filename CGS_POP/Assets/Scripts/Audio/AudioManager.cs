9aa893270d48373678b28a3589c988e1b15a235a
Matthew Churchyard <Matthew2.Churchyard@live.uwe.ac.uk>
2019-02-26 23:44:30 +0000

    Added Audio Manager

---
+++ CGS_POP/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public AudioSource abilitySound;
+	public AudioSource levelMusic;
+	public AudioSource effectsSound;
+	public static AudioManager instance = null;
+	[SerializeField] List<AudioClip> audioClips;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this) {
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+
+	}
+
+	public void PlayAbilitySound(AudioClip soundClip) {
+		abilitySound.clip = soundClip;
+		abilitySound.Play();
+	}
+
+	public void PlayEffectSound(AudioClip effectClip){
+		effectsSound.clip = effectClip;
+		effectsSound.Play();
+	}
+
+	public void PlayLevelMusic(AudioClip levelMusicClip)
+	{
+		levelMusic.clip = levelMusicClip;
+		levelMusic.Play();
+		levelMusic.loop = true;
+	}
+}
