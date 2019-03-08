70608cbd30ab2f15bb7dae0ab66c8b9900350d1a
AnimeAdam <Adam.Szlamp@gmail.com>
2019-03-08 02:02:33 +0000

    Quick Debug Menu

---
+++ CGS_POP/Assets/Scripts/Debug/DebugLevelClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DebugLevelClick: MonoBehaviour
+{
+
+    public void GoToScene(int _scene)
+    {
+        SceneManager.LoadScene(_scene);
+    }
+
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Home))
+        //{
+        //    Debug.Log("You are debugging :)");
+        //    gameObject.SetActive(true);
+        //}
+        //if (Input.GetKeyDown(KeyCode.End))
+        //{
+        //    Debug.Log("You are debugging :)");
+        //    gameObject.SetActive(false);
+        //}
+    }
+}
---
+++ CGS_POP/Assets/Scripts/Debug/DebugUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugUI : MonoBehaviour
+{
+    GameObject _debugMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _debugMenu = GameObject.Find("DebugMenu");
+        _debugMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Home))
+        {
+            Debug.Log("You are debugging :)");
+            _debugMenu.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.End))
+        {
+            Debug.Log("You are debugging :)");
+            _debugMenu.SetActive(false);
+        }
+    }
+}
