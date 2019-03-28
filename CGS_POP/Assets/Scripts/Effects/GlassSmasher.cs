1fe415cd1277c0883ac30a24b8bbd8bab7ff6816
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2019-03-28 15:15:04 +0000

    glass will smash if a triangle touches it

---
+++ CGS_POP/Assets/Scripts/Effects/GlassSmasher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassSmasher : MonoBehaviour
+{
+    public ParticleSystem shardParticles;
+    bool particleSpawned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(particleSpawned == false) { 
+        string shapeName = other.name;
+            if (shapeName.Contains("Triangle"))
+            {
+                Instantiate(shardParticles, this.transform.position, Quaternion.Euler(0, 0, 0));
+                particleSpawned = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
