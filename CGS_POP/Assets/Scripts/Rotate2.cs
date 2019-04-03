478b8bc953dfe37797b4b2eb53ec350a655532f9
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2019-04-03 20:21:11 +0100

    Added Saws to FlipPool

---
+++ CGS_POP/Assets/Scripts/Rotate2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate2: MonoBehaviour
+{
+    public int speed = 5;
+    Vector3 movement = new Vector3(0, 0, 1);
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(movement * speed);
+    }
+}
