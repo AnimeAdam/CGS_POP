745159aaadf2bd1ec93b6a712311141ece647f91
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2019-04-03 14:05:54 +0100

    Sparkles are now more sparkly

--- CGS_POP/Assets/Scripts/RotationLock.cs
+++ CGS_POP/Assets/Scripts/RotationLock.cs
@@ -15,6 +15,6 @@ public class RotationLock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        particleTransform.position = lockedRot;
+        particleTransform.rotation = Quaternion.Euler(lockedRot);
     }
 }
