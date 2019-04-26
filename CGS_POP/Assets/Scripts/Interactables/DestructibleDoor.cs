fbe40ecb709f843f6f68e610df913ab1417d82fa
Matthew <Matthew2.Churchyard@live.uwe.ac.uk>
2019-04-26 20:11:40 +0100

    Colliding a shape with the basket breaks a door and that basket. break all to progress

---
+++ CGS_POP/Assets/Scripts/Interactables/DestructibleDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleDoor : MonoBehaviour
+{
+    public bool unlockDoor = false;
+    public ParticleSystem unlockParticle;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (unlockDoor == true) {
+            Instantiate(unlockParticle, transform.position, Quaternion.Euler(0, 0, 0));
+            Destroy(gameObject);
+        }
+    }
+
+}
+
---
+++ CGS_POP/Assets/Scripts/Interactables/DoorDestroyer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDestroyer : MonoBehaviour
+{
+    public DestructibleDoor desDor;
+    public ParticleSystem unlockParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Red" || other.tag == "Blue" || other.tag == "Green" || other.tag == "Yellow")
+        {
+            desDor.unlockDoor = true;
+            Instantiate(unlockParticle, transform.position, Quaternion.Euler(0, 0, 0));
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
