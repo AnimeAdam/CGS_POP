30a1d87e11c418eacbad73b516a737491e8b9504
Adam Szlamp <Adam.Szlamp@gmail.com>
2019-04-30 17:17:03 +0100

    floating fixed

--- CGS_POP/Assets/Scripts/Shapes/ShapeManager.cs
+++ CGS_POP/Assets/Scripts/Shapes/ShapeManager.cs
@@ -331,7 +331,8 @@ public class ShapeManager : MonoBehaviour
                 Vector3 _vec3 = _ts.position;
                 _vec3.y += floatSpeed * Time.deltaTime;
                 _ts.GetComponent<Rigidbody>().useGravity = false;
-				originalConstraints = _ts.GetComponent<Rigidbody>().constraints;
+                _ts.GetComponent<Rigidbody>().Sleep();
+                originalConstraints = _ts.GetComponent<Rigidbody>().constraints;
 				_ts.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 				_ts.position = _vec3;
             }
